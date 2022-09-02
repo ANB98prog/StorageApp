f2aa56cfbdb0d3e92aeda201676a74bae1444665
Alex Newman <bakaev_a1998@mail.ru>
2022-09-02 10:02:42 +0300

    Created Storage.Backend project

---
+++ Storage.Backend/Storage.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Storage.Application.Common.Mappings
+{
+    /// <summary>
+    /// Assebles mapping profiles
+    /// </summary>
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+
+        /// <summary>
+        /// Applies mapping from assembly
+        /// </summary>
+        /// <param name="assembly">Assembly</param>
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                        .Where(t => t.GetInterfaces()
+                            .Any(i => i.IsGenericType 
+                                && i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                        .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
---
+++ Storage.Backend/Storage.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Storage.Application.Common.Mappings
+{
+    /// <summary>
+    /// Interface of mapping configuration
+    /// </summary>
+    /// <typeparam name="T">Map with type</typeparam>
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
