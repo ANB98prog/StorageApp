56c9bf65676755dc16eb9e33b65f2a20f5ec330f
Alex Newman <bakaev_a1998@mail.ru>
2022-09-02 11:35:29 +0300

    Added base controller class

---
+++ Storage.Backend/Storage.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Storage.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BaseController : ControllerBase
+    {
+        internal Guid UserId => !User.Identity.IsAuthenticated 
+                                    ? Guid.Empty 
+                                        : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+    }
+}
