9081bb40c18dff0f903777b41a4b58dd75f5549d
Alex Newman <bakaev_a1998@mail.ru>
2022-09-02 11:35:43 +0300

    Added exception handler middleware

---
+++ Storage.Backend/Storage.WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Storage.WebApi.Middleware
+{
+    /// <summary>
+    /// Custom exception handler middleware
+    /// </summary>
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next) => 
+            _next = next;
+
+        /// <summary>
+        /// Invokes next request in middleware
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles exception during middleware
+        /// </summary>
+        /// <param name="context">Request context</param>
+        /// <param name="exception">Exception while processing</param>
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+            switch (exception)
+            {
+                 //Handle exceptions
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            if(result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { errpr = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ Storage.Backend/Storage.WebApi/Middleware/CustomExceptionHandlerMiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace Storage.WebApi.Middleware
+{
+    public static class CustomExceptionHandlerMiddlewareExtension
+    {
+        /// <summary>
+        /// Includes custom exception handler middleware to pipline
+        /// </summary>
+        /// <param name="builder">Application buider</param>
+        /// <returns>Application buider</returns>
+        public static IApplicationBuilder UseCustomExceptionHandler(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+        }
+    }
+}
