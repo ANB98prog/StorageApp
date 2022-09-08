d03e010007c6db948f33fa8e385a929e45d4bbe4
Alex Newman <bakaev_a1998@mail.ru>
2022-09-08 12:43:44 +0300

    Moved some Image domain fields to BaseFile model

---
+++ Storage.Backend/Storage.Domain/BaseFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Storage.Domain
+{
+    public class BaseFile
+    {
+        /// <summary>
+        /// Image id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Image owner id
+        /// </summary>
+        public Guid OwnerId { get; set; }
+
+        /// <summary>
+        /// Image department owner id
+        /// </summary>
+        public Guid DepartmentOwnerId { get; set; }
+
+        /// <summary>
+        /// Original image name
+        /// </summary>
+        public string OriginalName { get; set; }
+
+        /// <summary>
+        /// System image name
+        /// </summary>
+        public string SystemName { get; set; }
+
+        /// <summary>
+        /// File extension
+        /// </summary>
+        public string FileExtension { get; set; }
+
+        /// <summary>
+        /// Content type
+        /// </summary>
+        public string ContentType { get; set; }
+
+        /// <summary>
+        /// Original file path
+        /// </summary>
+        public string OriginalFilePath { get; set; }
+
+        /// <summary>
+        /// Image attributes
+        /// </summary>
+        public IEnumerable<string> Attributes { get; set; }
+
+        /// <summary>
+        /// Created at
+        /// </summary>
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+        /// <summary>
+        /// Edited at
+        /// </summary>
+        public DateTime EditedAt { get; set; }
+    }
+}
