7efd26ffc8235941e86cf051a745ce2c8ac7ad48
Alex Newman <bakaev_a1998@mail.ru>
2022-09-08 12:44:01 +0300

    Added Dependency injection extension for application

---
+++ Storage.Backend/Storage.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Storage.Application.Common.Behaviors;
+using System.Reflection;
+
+namespace Storage.Application
+{
+    /// <summary>
+    /// Application dependency injection extension
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Adds application dependencies
+        /// </summary>
+        /// <param name="services">Services collection</param>
+        /// <returns>Services collection</returns>
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
