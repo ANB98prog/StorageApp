87bb6aa00e71d9eba32f0d3e409cd1aa100d45e0
Alex Newman <bakaev_a1998@mail.ru>
2022-09-08 11:53:06 +0300

    Added upload image command

---
+++ Storage.Backend/Storage.Application/Images/Commands/UploadImage/UploadImageCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+
+namespace Storage.Application.Images.Commands.UploadImage
+{
+    public class UploadImageCommand : IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+
+        public List<string> Attributes { get; set; } = new List<string>();
+
+        public IFormFile ImageFile { get; set; }
+    }
+}
---
+++ Storage.Backend/Storage.Application/Images/Commands/UploadImage/UploadImageCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Storage.Application.Images.Commands.UploadImage
+{
+    public class UploadImageCommandHandler
+        : IRequestHandler<UploadImageCommand, Guid>
+    {
+        public Task<Guid> Handle(UploadImageCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
