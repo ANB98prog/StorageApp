55b5f97e4a882f590b6783532ceb8c3ff8f6f72c
Alex Newman <bakaev_a1998@mail.ru>
2022-09-08 12:09:30 +0300

    Added Imange domain model

---
+++ Storage.Backend/Storage.Domain/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Storage.Domain
+{
+    /// <summary>
+    /// Organization department
+    /// </summary>
+    public class Department
+    {
+        /// <summary>
+        /// Department id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Department name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Department description
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ Storage.Backend/Storage.Domain/Image.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Storage.Domain
+{
+    /// <summary>
+    /// Image domain model
+    /// </summary>
+    public class Image
+    {
+        /// <summary>
+        /// Image id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Image owner id
+        /// </summary>
+        public Guid OwnerId { get; set; }
+
+        /// <summary>
+        /// Image department owner id
+        /// </summary>
+        public Guid DepartmentOwnerId { get; set; }
+
+        /// <summary>
+        /// Original image name
+        /// </summary>
+        public string OriginalName { get; set; }
+
+        /// <summary>
+        /// System image name
+        /// </summary>
+        public string SystemName { get; set; }
+
+        /// <summary>
+        /// File extension
+        /// </summary>
+        public string FileExtension { get; set; }
+
+        /// <summary>
+        /// Original file path
+        /// </summary>
+        public string OriginalFilePath { get; set; }
+
+        /// <summary>
+        /// Compressed file path
+        /// </summary>
+        public string CompressedFilePath { get; set; }
+
+        /// <summary>
+        /// Image attributes
+        /// </summary>
+        public IEnumerable<string> Attributes { get; set; }
+
+        /// <summary>
+        /// Created at
+        /// </summary>
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+        /// <summary>
+        /// Edited at
+        /// </summary>
+        public DateTime EditedAt { get; set; }
+    }
+}
