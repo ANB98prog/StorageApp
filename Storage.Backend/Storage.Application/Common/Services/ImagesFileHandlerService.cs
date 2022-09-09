4c4f937b0d5e5b5ae6ed4a2d3c45aa2af163878d
Alex Newman <bakaev_a1998@mail.ru>
2022-09-09 15:31:28 +0300

    Implemented ImagesFileHandlerService download method

---
+++ Storage.Backend/Storage.Application/Common/Services/ImagesFileHandlerService.cs
@@ -0,0 +1,51 @@
+﻿using Storage.Application.Common.Helpers;
+using Storage.Application.Common.Models;
+using Storage.Application.Interfaces;
+using Storage.Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace Storage.Application.Common.Services
+{
+    public class ImagesFileHandlerService : IFileHandlerService
+    {
+        private readonly IFileService _fileService;
+
+        public ImagesFileHandlerService(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        public Task<FileStream> DownloadFileAsync(string filePath, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DownloadManyFilesAsync(List<string> filesPath, CancellationToken cancellationToken)
+        {
+            //Подготавливаем файлы
+                // Создаем временную папку
+                // Архивируем ее
+
+            var tempDir = Path.GetRandomFileName();
+
+            var zipFile = FileHelper.MoveFilesToAsync(filesPath, tempDir);
+        }
+
+        public Task<Guid> UploadFileAsync<T>(T file, CancellationToken cancellationToken) where T : BaseFile
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Task> UploadLargeFileAsync<T>(T file, CancellationToken cancellationToken) where T : BaseFile
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
