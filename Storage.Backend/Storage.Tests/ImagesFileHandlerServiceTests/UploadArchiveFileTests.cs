217b49e550467b85ecf2dc1a3607efbb2c142352
Alex Newman <bakaev_a1998@mail.ru>
2022-09-13 16:37:51 +0300

    Added tests for UploadFile method

--- Storage.Backend/Storage.Tests/ImagesFileHandlerServiceTests/UploadArchiveFileTests.cs
+++ Storage.Backend/Storage.Tests/ImagesFileHandlerServiceTests/UploadArchiveFileTests.cs
@@ -1,11 +1,6 @@
 ﻿using Storage.Application.Common.Helpers;
 using Storage.Application.Common.Models;
 using Storage.Tests.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Storage.Tests.ImagesFileHandlerServiceTests
 {
---
+++ Storage.Backend/Storage.Tests/ImagesFileHandlerServiceTests/UploadFileTests.cs
@@ -0,0 +1,52 @@
+﻿using Storage.Application.Common.Models;
+using Storage.Application.Common.Helpers;
+using Storage.Tests.Common;
+
+namespace Storage.Tests.ImagesFileHandlerServiceTests
+{
+    [Collection("TestServicesCollection")]
+    public class UploadFileTests : TestServicesFixture
+    {
+        [Fact]
+        public async Task UploadFile_Success()
+        {
+            var dir = Path.Combine(TestFilesDirectory, "UploadFile");
+
+            Directory.CreateDirectory(dir);
+
+            File.WriteAllText(Path.Combine(dir, "upload.txt"), "text");
+            var file = File.Open(Path.Combine(dir, "upload.txt"), FileMode.Open);
+
+            var id = Guid.NewGuid();
+            var request = new UploadFileRequestModel
+            {
+                Id = id,
+                SystemName = $"{id.Trunc()}.txt",
+                Stream = file,
+            };
+
+            var result = await FileHandlerService.UploadFileAsync(request, CancellationToken.None);
+
+            Assert.Equal(id, result);
+        }
+
+        [Fact]
+        public async Task UploadFile_Error_IfRequestIsNull()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>( async () =>
+                await FileHandlerService.UploadFileAsync(null, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task UploadFile_Error_IfFileStreamIsNull()
+        {
+            var error = await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                            await FileHandlerService.UploadFileAsync(new UploadFileRequestModel
+                            {
+                                SystemName = $"{Guid.NewGuid().Trunc()}.txt"
+                            }, CancellationToken.None));
+
+            Assert.Equal("stream", error.ParamName);
+        }
+    }
+}
