82aca0be8bab2ab7ef2ebd7b2a0d348261f8b4f1
Alex Newman <bakaev_a1998@mail.ru>
2022-09-13 12:09:01 +0300

    Added DownloadManyFiles tests for ImageFilesHandlerService

---
+++ Storage.Backend/Storage.Tests/ImagesFileHandlerServiceTests/DownloadManyFilesTests.cs
@@ -0,0 +1,78 @@
+﻿using Storage.Application.Common.Helpers;
+using Storage.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Tests.ImagesFileHandlerServiceTests
+{
+    [Collection("TestServicesCollection")]
+    public class DownloadManyFilesTests : TestServicesFixture
+    {
+        [Fact]
+        public async Task DownloadManyFiles_Success()
+        {
+            var dir = Path.Combine(TestFilesDirectory, "HandleManyDownload");
+
+            Directory.CreateDirectory(dir);
+
+            var files = new List<string>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                var filePath = Path.Combine(dir, $"many_dw_{i}.txt");
+                File.WriteAllText(filePath, i.ToString());
+
+                files.Add(filePath);
+            }
+
+            var result = await FileHandlerService.DownloadManyFilesAsync(files, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal(".zip", Path.GetExtension(result.Name));
+
+            var unzippedPath = Path.Combine(TestFilesDirectory, "UnzippedMany");
+
+            Directory.CreateDirectory(unzippedPath);
+
+            await FileHelper.SaveFileAsync(result, Path.Combine(unzippedPath, Path.GetFileName(result.Name)), CancellationToken.None);
+
+            unzippedPath = FileHelper.UnzipFolder(Path.Combine(unzippedPath, Path.GetFileName(result.Name)));
+
+            Assert.True(Directory.Exists(unzippedPath));
+
+            var filesCount = Directory.GetFiles(unzippedPath).Count();
+
+            Assert.Equal(3, filesCount);
+
+            result.Dispose();
+        }
+
+        [Fact]
+        public async Task DownloadManyFiles_Error_IfFilesPathNull()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await FileHandlerService.DownloadManyFilesAsync(null, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task DownloadManyFiles_Error_IfFilesPathEmpty()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+                await FileHandlerService.DownloadManyFilesAsync(new List<string>(), CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task DownloadManyFiles_Error_IfFilesNotFound()
+        {
+            var filePath = Path.Combine(TestFilesDirectory, "notExisted.txt");
+            var message = await Assert.ThrowsAsync<FileNotFoundException>(async () =>
+                await FileHandlerService.DownloadManyFilesAsync(new List<string>() { filePath }, CancellationToken.None));
+
+            Assert.Equal(Path.GetFileName(filePath), message.FileName);
+        }
+    }
+}
