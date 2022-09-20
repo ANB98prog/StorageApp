3aca968b8a7ce160dc83a5c8684ab3e8fc98dd29
Alex Newman <bakaev_a1998@mail.ru>
2022-09-20 16:58:12 +0300

    Implemented create index method in elastic client

---
+++ Storage.Backend/Elasticsearch/Exceptions/ErrorMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace Elasticsearch.Exceptions
+{
+    /// <summary>
+    /// Error messages
+    /// </summary>
+    public static class ErrorMessages
+    {
+        public const string INDEX_ALREADY_EXISTS = "Index already exists!";
+
+        public static string ERROR_CREATING_INDEX(string indexName) => $"Error occured while index creating. Index name '{indexName}'";
+    }
+}
---
+++ Storage.Backend/Elasticsearch/Exceptions/IndexCreationException.cs
@@ -0,0 +1,53 @@
+﻿namespace Elasticsearch.Exceptions
+{
+    /// <summary>
+    /// Error that describes elastic index creation
+    /// </summary>
+    public class IndexCreationException : Exception
+    {
+        /// <summary>
+        /// Index name
+        /// </summary>
+        public string IndexName { get; private set; }
+
+        /// <summary>
+        /// User friendly message
+        /// </summary>
+        public string UserfriendlyMessage { get; private set; }
+
+        /// <summary>
+        /// Creates exception of <see cref="IndexCreationException"/>
+        /// </summary>
+        /// <param name="indexName">Index name</param>
+        /// <param name="innerException">Inner exception</param>
+        public IndexCreationException(string indexName, Exception innerException)
+            : base(ErrorMessages.ERROR_CREATING_INDEX(indexName), innerException)
+        {
+            IndexName = indexName;
+            UserfriendlyMessage = ErrorMessages.ERROR_CREATING_INDEX(indexName);            
+        }
+
+        /// <summary>
+        /// Creates exception of <see cref="IndexCreationException"/>
+        /// </summary>
+        /// <param name="indexName">Index name</param>
+        public IndexCreationException(string indexName, string message)
+            : base(message)
+        {
+            IndexName = indexName;
+            UserfriendlyMessage = message;
+        }
+
+        /// <summary>
+        /// Creates exception of <see cref="IndexCreationException"/>
+        /// </summary>
+        /// <param name="indexName">Index name</param>
+        /// <param name="innerException">Inner exception</param>
+        public IndexCreationException(string indexName, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            IndexName = indexName;
+            UserfriendlyMessage = message;
+        }
+    }
+}
