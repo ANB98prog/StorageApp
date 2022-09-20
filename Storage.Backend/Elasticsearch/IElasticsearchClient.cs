9904dac39cc24cd6c5b432851fe77d27ebbd3631
Alex Newman <bakaev_a1998@mail.ru>
2022-09-20 16:57:47 +0300

    Added Elasticsearch client interface

---
+++ Storage.Backend/Elasticsearch/IElasticsearchClient.cs
@@ -0,0 +1,22 @@
+﻿using Nest;
+
+namespace Elasticsearch
+{
+    public interface IElasticsearchClient
+    {
+        /// <summary>
+        /// Creates index
+        /// </summary>
+        /// <param name="indexName">Index name</param>
+        /// <param name="descriptor">Description of index</param>
+        /// <returns></returns>
+        public Task CreateIndexAsync(string indexName, Func<CreateIndexDescriptor, ICreateIndexRequest> descriptor = null);
+
+        /// <summary>
+        /// Creates index
+        /// </summary>
+        /// <param name="indexName">Index name</param>
+        /// <returns></returns>
+        public Task CreateIndexAsync(string indexName);
+    }
+}
