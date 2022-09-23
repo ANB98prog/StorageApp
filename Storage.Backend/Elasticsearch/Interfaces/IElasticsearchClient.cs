763b1888cc7af5b58ce7baae6553598da64361c4
Alex Newman <bakaev_a1998@mail.ru>
2022-09-23 11:22:13 +0300

    Added methods to delete documents in IElasticsearch interface

--- Storage.Backend/Elasticsearch/Interfaces/IElasticsearchClient.cs
+++ Storage.Backend/Elasticsearch/Interfaces/IElasticsearchClient.cs
@@ -84,5 +84,38 @@ namespace Elasticsearch.Interfaces
 
         #endregion
 
+        #region Delete
+        /// <summary>
+        /// Deletes document
+        /// </summary>
+        /// <typeparam name="TDocument">Document type</typeparam>
+        /// <param name="index">Index to delete in</param>
+        /// <param name="documentId">Document to delete</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns></returns>
+        public Task DeleteDocumentAsync<TDocument>(string index, string documentId, CancellationToken cancellationToken = default) where TDocument : class;
+
+        /// <summary>
+        /// Deletes document
+        /// </summary>
+        /// <typeparam name="TDocument">Document type</typeparam>
+        /// <param name="index">Index to delete in</param>
+        /// <param name="ids">List of documents ids to delete</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns></returns>
+        public Task DeleteBulkAsync<TDocument>(string index, IEnumerable<string> ids, CancellationToken cancellationToken = default) where TDocument : class;
+
+        /// <summary>
+        /// Deletes document
+        /// </summary>
+        /// <typeparam name="TDocument">Document type</typeparam>
+        /// <param name="index">Index to delete in</param>
+        /// <param name="selector">Documents selector</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns></returns>
+        public Task DeleteBulkAsync<TDocument>(string index, Func<DeleteDescriptor<TDocument>, IDeleteRequest> selector, CancellationToken cancellationToken = default) where TDocument : class;
+
+        #endregion
+
     }
 }
