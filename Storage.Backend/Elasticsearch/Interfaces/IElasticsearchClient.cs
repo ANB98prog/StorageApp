58cc634b96692fbb136df3e9039d62bbd13a9129
Alex Newman <bakaev_a1998@mail.ru>
2022-09-23 10:59:48 +0300

    Moved IElasticSearchClient to another folder

--- Storage.Backend/Elasticsearch/Interfaces/IElasticsearchClient.cs
+++ Storage.Backend/Elasticsearch/Interfaces/IElasticsearchClient.cs
@@ -1,6 +1,6 @@
 ﻿using Nest;
 
-namespace Elasticsearch
+namespace Elasticsearch.Interfaces
 {
     public interface IElasticsearchClient
     {
@@ -12,7 +12,7 @@ namespace Elasticsearch
         /// <param name="descriptor">Description of index</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns></returns>
-        public Task CreateIndexAsync(string indexName, Func<CreateIndexDescriptor, ICreateIndexRequest> descriptor = null, CancellationToken cancellationToken = default(CancellationToken));
+        public Task CreateIndexAsync(string indexName, Func<CreateIndexDescriptor, ICreateIndexRequest> descriptor = null, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Creates index
@@ -20,7 +20,7 @@ namespace Elasticsearch
         /// <param name="indexName">Index name</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns></returns>
-        public Task CreateIndexAsync(string indexName, CancellationToken cancellationToken = default(CancellationToken));
+        public Task CreateIndexAsync(string indexName, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Deletes index
@@ -28,7 +28,7 @@ namespace Elasticsearch
         /// <param name="indexName">Index to delete</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns></returns>
-        public Task DeleteIndexAsync(string indexName, CancellationToken cancellationToken = default(CancellationToken));
+        public Task DeleteIndexAsync(string indexName, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Indexes document
@@ -37,7 +37,7 @@ namespace Elasticsearch
         /// <param name="document">Document to index</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns></returns>
-        public Task<Guid> IndexDocumentAsync<TDocument>(TDocument document, CancellationToken cancellationToken = default(CancellationToken)) where TDocument : class;
+        public Task<Guid> IndexDocumentAsync<TDocument>(TDocument document, CancellationToken cancellationToken = default) where TDocument : class;
 
         //// <summary>
         /// Indexes document
@@ -47,7 +47,7 @@ namespace Elasticsearch
         /// <param name="selector">Index selector</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns></returns>
-        public Task<Guid> IndexDocumentAsync<TDocument>(TDocument document, Func<IndexDescriptor<TDocument>, IIndexRequest<TDocument>> selector, CancellationToken cancellationToken = default(CancellationToken)) where TDocument : class;
+        public Task<Guid> IndexDocumentAsync<TDocument>(TDocument document, Func<IndexDescriptor<TDocument>, IIndexRequest<TDocument>> selector, CancellationToken cancellationToken = default) where TDocument : class;
 
         /// <summary>
         /// Indexes many documents
@@ -57,7 +57,7 @@ namespace Elasticsearch
         /// <param name="index">Index to add to</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>List of ids</returns>
-        public Task<IEnumerable<Guid>> IndexManyAsync<TDocument>(IEnumerable<TDocument> documents, string index = null, CancellationToken cancellationToken = default(CancellationToken)) where TDocument : class;
+        public Task<IEnumerable<Guid>> IndexManyAsync<TDocument>(IEnumerable<TDocument> documents, string index = null, CancellationToken cancellationToken = default) where TDocument : class;
 
         /// <summary>
         /// Indexes bulk of documents
@@ -67,7 +67,7 @@ namespace Elasticsearch
         /// <param name="index">Index to add to</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>List of ids</returns>
-        public Task<IEnumerable<Guid>> BulkDocuments<TDocument>(IEnumerable<TDocument> documents, string index, CancellationToken cancellationToken = default(CancellationToken)) where TDocument : class;
+        public Task<IEnumerable<Guid>> BulkDocuments<TDocument>(IEnumerable<TDocument> documents, string index, CancellationToken cancellationToken = default) where TDocument : class;
         #endregion
 
         #region Search
@@ -80,7 +80,7 @@ namespace Elasticsearch
         /// <param name="request">Search request</param>
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>Documents</returns>
-        public Task<Models.SearchResponse<TDocument>> SearchAsync<TDocument>(string index, SearchRequest<TDocument> request, CancellationToken cancellationToken = default(CancellationToken)) where TDocument : class;
+        public Task<Models.SearchResponse<TDocument>> SearchAsync<TDocument>(string index, SearchRequest<TDocument> request, CancellationToken cancellationToken = default) where TDocument : class;
 
         #endregion
 
