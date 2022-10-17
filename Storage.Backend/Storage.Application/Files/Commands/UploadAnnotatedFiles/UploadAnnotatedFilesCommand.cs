7cd4a4c393414d97f08e7012a444599a432e5e43
Alex Newman <bakaev_a1998@mail.ru>
2022-10-17 17:13:09 +0300

    Added upload annotated files command

---
+++ Storage.Backend/Storage.Application/Files/Commands/UploadAnnotatedFiles/UploadAnnotatedFilesCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Application.Files.Commands.UploadAnnotatedFiles
+{
+    public class UploadAnnotatedFilesCommand : IRequest
+    {
+
+    }
+}
