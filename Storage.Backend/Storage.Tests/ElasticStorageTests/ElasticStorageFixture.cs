0e1e8579223f9ef7109142c8d9a87a7d1a043b70
Alex Newman <bakaev_a1998@mail.ru>
2022-10-25 13:08:25 +0300

    Fixed tests

--- Storage.Backend/Storage.Tests/ElasticStorageTests/ElasticStorageFixture.cs
+++ Storage.Backend/Storage.Tests/ElasticStorageTests/ElasticStorageFixture.cs
@@ -1,8 +1,11 @@
-﻿using Elasticsearch.Interfaces;
+﻿using AutoMapper;
+using Elasticsearch.Interfaces;
 using Moq;
+using Ninject;
 using Serilog;
 using Storage.Application.Common.Services;
 using Storage.Application.Interfaces;
+using Storage.Tests.Common;
 
 namespace Storage.Tests.ElasticStorageTests
 {
@@ -10,9 +13,12 @@ namespace Storage.Tests.ElasticStorageTests
     {
         public IStorageDataService GetElasticStorageService(Mock<IElasticsearchClient> elasticClientMock)
         {
+            var ioc = new IoCModule();
+            var kernel = new StandardKernel(ioc);
+
             var loggerMock = new Mock<ILogger>();
 
-            return new ElasticStorageService("test_files", loggerMock.Object, elasticClientMock.Object);
+            return new ElasticStorageService("test_files", loggerMock.Object, kernel.Get<IMapper>(), elasticClientMock.Object);
         }
 
         public void Dispose()
